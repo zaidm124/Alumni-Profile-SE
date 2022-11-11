9a565a833735143ca63e43023444b600e7eeefea
Zaid Bhimala <zaidm124@gmail.com>
2022-11-11 14:55:54 +0530

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      Welcome to Alumni Profile Website
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
     </div>
   );
 }
