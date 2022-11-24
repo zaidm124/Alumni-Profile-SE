0fb0a7cf636dac2625e96504fd8d77d4f35f2b18
Tushar <tushar55755575@gmail.com>
2022-11-24 22:50:45 +0530

    Search Alumini List add

--- client/src/components/AlumniList.js
+++ client/src/components/AlumniList.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-// import "./Searchalumni.css";
+import "./AluminiList.css";
 // import db from "../FireBase";
 
 function SearchAlumni() {
-  const [final, setFinal] = useState([]);
-  const [search, setSearch] = useState([]);
+  // const [final, setFinal] = useState([]);
+  // const [search, setSearch] = useState([]);
 //   useEffect(() => {
 //     db.collection("institute").onSnapshot((snapshhot) => {
 //       setSearch(
@@ -15,15 +15,15 @@ function SearchAlumni() {
 //       );
 //     });
 //   }, []);
-  console.log(search);
+  // console.log(search);
 
-  useEffect(() => {
-    setFinal(
-      search.filter((item) => item.data?.batch === localStorage.getItem("id"))
-    );
-  }, [search]);
-  console.log(final);
-  console.log(localStorage.getItem("id"));
+  // useEffect(() => {
+  //   setFinal(
+  //     search.filter((item) => item.data?.batch === localStorage.getItem("id"))
+  //   );
+  // }, [search]);
+  // console.log(final);
+  // console.log(localStorage.getItem("id"));
 
   // return final.map((s) => {
   return (
@@ -33,34 +33,34 @@ function SearchAlumni() {
           <div className="Title">
             <h2>Search Results</h2>
           </div>
-          {final.map((s) => {
-            return (
+          {/* {final.map((s) => {
+            return ( */}
               <>
                 <a
                   href="/vieweralumni"
-                  onClick={() =>
-                    localStorage.setItem("admission", s.data?.admission)
-                  }
+                  // onClick={() =>
+                  //   localStorage.setItem("admission", s.data?.admission)
+                  // }
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <div className="ProfileBackground">
                     <div className="Profile">
                       <div>
-                        <h3>Name:{s.data?.name}</h3>
+                        <h3>Name: s.data?.name</h3>
                       </div>
                       <div>
-                        <h3>Batch:{s.data?.batch}</h3>
+                        <h3>Batch: s.data?.batch</h3>
                       </div>
                       <div>
-                        <h3>Addmission No:{s.data?.admission}</h3>
+                        <h3>Addmission No: s.data?.admission</h3>
                       </div>
                     </div>
                   </div>
                 </a>
               </>
-            );
-          })}
+            {/* ); */}
+          {/* })} */}
         </div>
       </div>
     </div>
